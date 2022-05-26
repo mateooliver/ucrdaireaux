bf814fcd170628da6941ca808ac349f85e4641b4
Mateo Oliver <mateooliver@MacBook-Air-de-Mateo.local>
2022-05-26 15:37:39 -0300

    pruba 2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,7 +6,7 @@ import Carousel from 'react-bootstrap/Carousel'
 const CarouselIndex = () => {
   return (
     <div>
-        <Carousel className=' mx-auto my-3' style={{width:"73.5vw"}}>
+        <Carousel className=' mx-auto my-3' style={{width:"73.8vw"}}>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
